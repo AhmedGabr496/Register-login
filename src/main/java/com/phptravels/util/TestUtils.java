0cf587ad97d0d2f2d39508a46d960ac5d5402c5c
AhmedGabr496 <62850013+AhmedGabr496@users.noreply.github.com>
2020-03-30 17:53:52 +0200

    Add files via upload

--- src/main/java/com/phptravels/util/TestUtils.java
+++ src/main/java/com/phptravels/util/TestUtils.java
@@ -1,17 +1,15 @@
 package com.phptravels.util;
 
-//import atu.testrecorder.ATUTestRecorder;
-//import atu.testrecorder.exceptions.ATUTestRecorderException;
 import com.phptravels.base.TestBase;
 import org.apache.commons.io.FileUtils;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+
 
 public class TestUtils extends TestBase {
     public TestUtils() throws IOException
@@ -23,8 +21,6 @@ public class TestUtils extends TestBase {
         File srcFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
         FileUtils.copyFile(srcFile,new File("Reports\\"+name+".png"));
     }
-
-
     public static Object[][] getDataFromExcel(String SheetName) throws IOException
     {
         File testData= new File("src/main/java/com/phptravels/testdata/testData.xlsx");
